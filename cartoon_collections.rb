9d9be4a4347b9e4a6ccaddd8d4921a8966cbd2e2
Alexander Carlin <ahcarlin@gmail.com>
2018-10-07 05:17:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,5 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  if array.include?("cheddar" || "gouda" || "camembert")
-    return "cheddar"
-  else
-    return nil 
-  end
+  array.index{|c| c.include?("cheddar" || "gouda" || "camembert")}
 end
